af9a8bda6732ed666b7a090bb08b792c74a05211
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-06 17:19:53 +0100

    add-funcion featch to getEmailS

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ console.log('js')
 
 const list_container = document.getElementById('list_container')
 const reload_btn = document.getElementById('reload_btn')
+
+function getEmail() {
+   fetch ('https://flynn.boolean.careers/exercises/api/random/mail')
